9c8c13f62125be765f90f0d04e6870c199feb77e
Yassine37199 <yassine.ouri7319@gmail.com>
2022-06-12 19:41:42 +0100

    initialized Amazon S3 Configutations

---
+++ src/main/java/bucket/BucketName.java
@@ -0,0 +1,16 @@
+package bucket;
+
+public enum BucketName {
+    PROFILE_IMAGE("spring-image-upload-7319");
+
+    private final String bucketName;
+
+
+    BucketName(String bucketName) {
+        this.bucketName = bucketName;
+    }
+
+    public String getBucketName() {
+        return bucketName;
+    }
+}
---
+++ src/main/java/com/example/awsimageupload/AwsImageUploadApplication.java
@@ -0,0 +1,15 @@
+package com.example.awsimageupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsImageUploadApplication {
+
+	public static void main(String[] args) {
+
+	    SpringApplication.run(AwsImageUploadApplication.class, args);
+	    System.out.println("It Works !!!");
+	}
+
+}
